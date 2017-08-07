c3b430fa3c5c83b796f723e370f222e99e6c1c65
Emanuel Imhof <hello@m43nu.ch>
2017-08-07 14:04:53 +0200

    Extend the after_assign hook of the image_file accessor with the optimize process

--- app/models/alchemy/picture_extension.rb
+++ app/models/alchemy/picture_extension.rb
@@ -1,5 +1,11 @@
 Alchemy::Picture.class_eval do
-  image_accessor :image_file do
-    after_assign { |a| a.process!(:optimize) }
+  # Enables Dragonfly image processing
+  dragonfly_accessor :image_file, app: :alchemy_pictures do
+    # Preprocess after uploading the picture
+    after_assign do |p|
+      resize = Config.get(:preprocess_image_resize)
+      p.thumb!(resize) if resize.present?
+      p.process!(:optimize)
+    end
   end
 end
