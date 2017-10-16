455b2cb482353a0dffddeac9c61c1f230cfbeb62
Cristian Livadaru <cristian@lcx.at>
2017-10-16 14:51:47 +0200

    update for alchemy 4 and use params from image_optim setup

--- alchemy-image_optim.gemspec
+++ alchemy-image_optim.gemspec
@@ -20,7 +20,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "image_optim"
-  spec.add_dependency "alchemy_cms", "~> 3.6"
+  spec.add_dependency "alchemy_cms", "> 3.6"
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rspec-rails"
--- lib/alchemy/image_optim/processor.rb
+++ lib/alchemy/image_optim/processor.rb
@@ -6,7 +6,7 @@ module Alchemy
           # We have to use a Tempfile which copy the optimized image because optimized correspond to a Tempfile path which may be deleted
           tempfile = Tempfile.new('optimized-image')
           begin
-            if (optimized = ::ImageOptim.new.optimize_image(content.path).presence)
+            if (optimized = ::ImageOptim.new(Rails.application.config.assets.image_optim).optimize_image(content.path).presence)
               optimized.copy(tempfile.path)
               content.update tempfile.read
             end
